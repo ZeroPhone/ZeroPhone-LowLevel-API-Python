e1666eb397976985d33e487327efad2673b2429f
CRImier <crimier@yandex.ru>
2017-11-14 14:43:29 +0200

    Added 5V DC-DC control support

--- zerophone_hw.py
+++ zerophone_hw.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-__all__ = ['get_hw_version_str', 'is_charging', 'RGB_LED']
+__all__ = ['get_hw_version_str', 'is_charging', 'RGB_LED', 'USB_DCDC']
 __version__ = '0.1'
 
 import sys
@@ -25,6 +25,46 @@ def is_charging():
         return bool(gpio.input(chg_sense_gpio))
     else:
         raise NotImplementedException("Version not supported!")
+
+
+class USB_DCDC():
+    gpio_exported = False
+    gpio_state = None
+
+    switch_types = {
+        "gamma":"gpio_inverted"}
+
+    def __init__(self):
+        self.hw_v = get_hw_version_str()
+        self.gpio_num = self.get_gpio_num()
+
+    def get_gpio_num(self):
+        if self.hw_v == "gamma":
+            return 510
+        else:
+            raise NotImplementedException("Hardware version not supported!")
+
+    def set_state(self, state):
+        if not self.gpio_exported:
+            gpio.setup(self.gpio_num, gpio.OUT)
+            self.gpio_exported = True
+        self.gpio_state = state
+        if self.switch_types[self.hw_v] == "gpio_inverted":
+            gpio.set(self.gpio_num, not state)
+        elif self.switch_types[self.hw_v] == "gpio":
+            gpio.set(self.gpio_num, state)
+        else:
+            raise NotImplementedException("DC-DC switch type not supported!")
+
+    def on(self):
+        self.set_state(True)
+
+    def off(self):
+        self.set_state(False)
+
+    def toggle(self):
+        self.set_state(not self.gpio_state)
+
 
 class RGB_LED():
     color_mapping = {
@@ -86,6 +126,7 @@ class RGB_LED():
 
 if __name__ == "__main__":
     led = RGB_LED()
+    dcdc = USB_DCDC()
     from time import sleep
     while True:
         print(is_charging())
